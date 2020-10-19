67b42f387e90c1ec1a2d4283e74efb89eef69627
Ray Messina <rmessina10@gmail.com>
2020-10-19 18:14:09 -0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000, pause: "false" }); // accesses carousel method, sets interval attribute to 2 sec
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+
+});
