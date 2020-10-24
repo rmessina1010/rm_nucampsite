52cdc2710e4ac45145feb8fb8ff38c372315b68b
Ray Messina <rmessina10@gmail.com>
2020-10-24 13:04:28 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,34 @@ $(function () {
         }
     });
 
+    // Week 4: A scaling DRY solution
+    $('a[id]').click((event) => {
+        //available buttons
+        let targetHash = {
+            loginButton: "loginModal",
+            reserveButton: "reserveModal"
+        }
+        //ID of the clicked button
+        let ButtonID = event.target.id;
+        if (targetHash[ButtonID]) {// if  button ID is not in hash , no action is taken
+            $('.modal').modal('hide');
+            $('#' + targetHash[ButtonID]).modal('show');
+        }
+    });
+
+
+    /**
+        //Week 4: Simpler answer
+        $('#loginButton').click(() => {
+            $('.modal').modal('hide');
+            $('#loginModal').modal('show');
+        });
+
+        $('#reserveButton').click(() => {
+            $('.modal').modal('hide');
+            $('#reserveModal').modal('show');
+        });
+    **/
 });
+
+
